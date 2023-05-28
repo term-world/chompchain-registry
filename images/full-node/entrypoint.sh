649e37aa59d5e2c25cdfc5176e5e2b8b8f5876d6
dluman <dluman@allegheny>
2023-05-28 17:02:09 -0400

    Adding working versions of basic images

--- images/full-node/entrypoint.sh
+++ images/full-node/entrypoint.sh
@@ -5,4 +5,4 @@
 COUCHDB_PASSWORD=$(echo -n $RANDOM | sha256sum)
 
 # pm2 task
-pm2-runtime /opt/server/nodes/ecosystem.config.js --only "validator, registry"
+pm2-runtime /opt/server/chompchain-node/nodes/ecosystem.config.js --only "validator, registry"
