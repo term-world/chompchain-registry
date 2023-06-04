faba3cdc3e9a365c03065df76506ed2faecba5b6
dluman <dluman@allegheny.edu>
2023-06-04 14:40:46 -0400

    Adding switch to gosu user

--- images/full-node/entrypoint.sh
+++ images/full-node/entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-# CouchDB setup
-
-COUCHDB_PASSWORD=$(echo -n $RANDOM | sha256sum)
+# CouchDB startup
+service couchdb start
 
 # pm2 task
 pm2-runtime /opt/server/chompchain-node/nodes/ecosystem.config.js --only "validator, registry"
+
+# Transfer away from root user
+gosu chompers /bin/bash
