258a42be974190b9c49e3bf52eba68d1733a8c7b
dluman <dluman@allegheny.edu>
2023-06-05 12:57:11 -0400

    Adding improvements to entrypoint

--- images/full-node/entrypoint.sh
+++ images/full-node/entrypoint.sh
@@ -2,9 +2,15 @@
 
 # CouchDB startup
 service couchdb start
+sleep 5
+
+# CouchDB API requests
+
+curl -X PUT --user admin:$COUCHDB_PASSWORD http://127.0.0.1:5984/blocks
+curl -X PUT --user admin:$COUCHDB_PASSWORD http://127.0.0.1:5984/contracts
 
 # pm2 task
-pm2-runtime /opt/server/chompchain-node/nodes/ecosystem.config.js --only "validator, registry"
+# pm2-runtime /opt/server/chompchain-node/nodes/ecosystem.config.js --only "validator, registry"
 
 # Transfer away from root user
 gosu chompers /bin/bash
--- images/reg-node/entrypoint.sh
+++ images/reg-node/entrypoint.sh
@@ -2,3 +2,5 @@
 
 # pm2 task
 pm2-runtime /opt/server/ecosystem.config.js --only "registry"
+
+gosu chompers /bin/bash
