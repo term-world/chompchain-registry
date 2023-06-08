bdb0992de2a18f68e656adf7b9b85994bd8e0dbf
dluman <dluman@allegheny.edu>
2023-06-08 11:32:15 -0400

    More fixes to improve image build compatibility

--- images/full-node/entrypoint.sh
+++ images/full-node/entrypoint.sh
@@ -1,8 +1,11 @@
-#!/bin/sh
+#!/bin/bash
+
+# Source the bashrc file to load ENV vars
+source ~/.bashrc
 
 # CouchDB startup
 service couchdb start
-sleep 10
+sleep 5
 
 # CouchDB API requests
 
@@ -10,7 +13,7 @@ curl -X PUT --user admin:$COUCHDB_PASSWORD http://127.0.0.1:5984/blocks
 curl -X PUT --user admin:$COUCHDB_PASSWORD http://127.0.0.1:5984/contracts
 
 # pm2 task
-# pm2-runtime /opt/server/chompchain-node/nodes/ecosystem.config.js --only "validator, registry"
+pm2-runtime /opt/server/chompchain-node/nodes/ecosystem.config.js --only "validator, registry"
 
 # Transfer away from root user
 gosu chompers /bin/bash
